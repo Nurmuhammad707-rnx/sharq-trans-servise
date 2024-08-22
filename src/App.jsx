7687036797b953bb16d499a636f0a44dac729fa5
Samandar <shomurodovhusan130@gmail.com>
2024-08-22 15:43:45 +0500

    t

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './Components/Header'
 import { Route, Routes } from 'react-router-dom'
-import { router } from './router/Index'
+import { router } from './router/index'
 
 const App = () => {
   return (
